5fb895e2b67f7cb0e6728ee242192e33b879da9b
박건희 <ghpark@anchors-biz.com>
2025-07-09 14:33:28 +0900

    경로 수정

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -81,7 +81,7 @@ function submitContent() {
         textData : popup.querySelector('textarea').value
     }
 
-    fetch("http://www.melloplace.com:8080/crudTest.html", {
+    fetch("http://www.melloplace.com:8080/crudTest", {
         method : "POST",
         headers : {
             "Content-Type" : "application/json"
@@ -100,7 +100,7 @@ function submitContent() {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    fetch("http://www.melloplace.com:8080/crudTest.html")
+    fetch("http://www.melloplace.com:8080/crudTest")
     .then(res => res.json())
     .then(dataList => {
         const resultListArea = document.querySelector('.result-list-area');
